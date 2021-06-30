c26e10bbe109e93a44c3d4e78d9d16855c32dbf4
L <ermek.rahimberdiev@gmail.com>
2021-06-30 11:08:17 +0600

    fix microsrevice timetable

--- src/app/pages/teacher-scheduler/teacher-scheduler.component.ts
+++ src/app/pages/teacher-scheduler/teacher-scheduler.component.ts
@@ -23,7 +23,7 @@ export class TeacherSchedulerComponent implements OnInit {
     }
 
     getAllData() {
-        this.teacherSchedulerService.getAllData('E2019_2020').subscribe(res => {
+        this.teacherSchedulerService.getAllData( 'E2019_2020').subscribe(res => {
             this.dataSource = res;
             console.log('All Schedule:', res);
         });
